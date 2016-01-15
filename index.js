aadbf47d76ba7ed34270d275ee87f3d2ba577645
2betop <liaoxuezhi@icloud.com>
2016-01-15 06:05:20 +0000

    based on https://github.com/fis-components/components/blob/master/packages/rc-util.json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./src/');
+module.exports = {
+  guid: require('./lib/guid'),
+  classSet: require('./lib/classSet'),
+  joinClasses: require('./lib/joinClasses'),
+  KeyCode: require('./lib/KeyCode'),
+  PureRenderMixin: require('./lib/PureRenderMixin'),
+  shallowEqual: require('./lib/shallowEqual'),
+  createChainedFunction: require('./lib/createChainedFunction'),
+  Dom: {
+    addEventListener: require('./lib/Dom/addEventListener'),
+    contains: require('./lib/Dom/contains')
+  },
+  Children: {
+    toArray: require('./lib/Children/toArray')
+  }
+};
